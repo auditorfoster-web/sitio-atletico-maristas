ea5db8e8c8950a07ea335881213ccdf20bbdd187
GitHub Action <action@github.com>
2026-06-24 21:24:52 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -2,8 +2,8 @@
 // Ultimo resultado jugado de cada serie (Atletico Maristas).
 window.RESULTADOS = [
   { serie:'Junior', fecha:'20/06/2026', local:'Atletico Marista Junior', gl:2, visita:'America Junior', gv:3, golesAM:[{n:'Javier Maldonado',g:1},{n:'Bastian Rocha',g:1}] },
+  { serie:'Senior', fecha:'20/06/2026', local:'Atletico Marista Senior', gl:0, visita:'Cd Improvisados Senior', gv:7 },
   { serie:'Super Senior', fecha:'07/06/2026', local:'Atletico Marista Super Senior', gl:0, visita:'Mapuches Super Senior', gv:1 },
   { serie:'Dorada', fecha:'07/06/2026', local:'Atletico Marista Dorados', gl:0, visita:'Mapuches Dorados', gv:1 },
-  { serie:'Diamante', fecha:'07/06/2026', local:'Atletico Marista Diamantes', gl:1, visita:'Stadio Italiano Diamantes', gv:2, golesAM:[{n:'Marco Perez',g:1}] },
-  { serie:'Senior', fecha:'06/06/2026', local:'Ac Lo Campino Senior', gl:2, visita:'Atletico Marista Senior', gv:0 }
+  { serie:'Diamante', fecha:'07/06/2026', local:'Atletico Marista Diamantes', gl:1, visita:'Stadio Italiano Diamantes', gv:2, golesAM:[{n:'Marco Perez',g:1}] }
 ];
