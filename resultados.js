e4c1ad2955a9065f2519a417e7e11df6b6d0de99
GitHub Action <action@github.com>
2026-07-07 20:08:43 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -3,7 +3,7 @@
 window.RESULTADOS = [
   { serie:'Super Senior', fecha:'05/07/2026', local:'Atletico Marista Super Senior', gl:1, visita:'Leyenda 216 Super Senior', gv:5, golesAM:[{n:'Francisco Aguero',g:1}] },
   { serie:'Dorada', fecha:'05/07/2026', local:'Atletico Marista Dorados', gl:4, visita:'America Dorados', gv:0, golesAM:[{n:'Hervy Aroca',g:1},{n:'Luis Ayala',g:1},{n:'Victor Garcia',g:1},{n:'Nicolas Petrovich',g:1}] },
+  { serie:'Diamante', fecha:'05/07/2026', local:'Atletico Marista Diamantes', gl:1, visita:'Universitario Diamantes', gv:0, golesAM:[{n:'Enrique Urbina',g:1}] },
   { serie:'Junior', fecha:'04/07/2026', local:'Atletico Marista Junior', gl:3, visita:'Alianza Junior', gv:0 },
-  { serie:'Senior', fecha:'04/07/2026', local:'Alianza Senior', gl:2, visita:'Atletico Marista Senior', gv:2, golesAM:[{n:'Daniel Candia',g:1},{n:'Marcelo Guerra',g:1}] },
-  { serie:'Diamante', fecha:'21/06/2026', local:'Atletico Marista Diamantes', gl:0, visita:'Alianza Diamantes', gv:1 }
+  { serie:'Senior', fecha:'04/07/2026', local:'Alianza Senior', gl:2, visita:'Atletico Marista Senior', gv:2, golesAM:[{n:'Daniel Candia',g:1},{n:'Marcelo Guerra',g:1}] }
 ];
