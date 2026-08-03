a8ab0364bec75d30f62e995a56648ab71c3b30a9
GitHub Action <action@github.com>
2026-08-03 21:04:38 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -3,5 +3,7 @@
 window.RESULTADOS = [
   { serie:'Super Senior', fecha:'12/07/2026', local:'Atletico Marista Super Senior', gl:2, visita:'Charkaplax Fc Super Senior', gv:2, golesAM:[{n:'Francisco Aguero',g:1},{n:'Jose Rubio',g:1}] },
   { serie:'Dorada', fecha:'12/07/2026', local:'Atletico Marista Dorados', gl:1, visita:'Juventus Dorados', gv:0, golesAM:[{n:'Cristian Cancino',g:1}] },
-  { serie:'Junior', fecha:'11/07/2026', local:'Atletico Marista Junior', gl:3, visita:'Ciclon Junior', gv:2 }
+  { serie:'Diamante', fecha:'12/07/2026', local:'Atletico Marista Diamantes', gl:2, visita:'Juventus Diamantes', gv:1, golesAM:[{n:'German Ugalde',g:2}] },
+  { serie:'Junior', fecha:'11/07/2026', local:'Atletico Marista Junior', gl:3, visita:'Ciclon Junior', gv:2, golesAM:[{n:'Baltazar Aroca',g:1},{n:'Nicolas Maldonado',g:1},{n:'Bastian Quilodran',g:1}] },
+  { serie:'Senior', fecha:'11/07/2026', local:'Ciclon Senior', gl:2, visita:'Atletico Marista Senior', gv:2, golesAM:[{n:'Ulises Galvez',g:1},{n:'Willy Jeno',g:1}] }
 ];
