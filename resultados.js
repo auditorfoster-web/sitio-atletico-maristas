b0b25ca9d52a848d37a9de80fb958744be1d0bbb
GitHub Action <action@github.com>
2026-08-11 14:58:28 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -5,5 +5,5 @@ window.RESULTADOS = [
   { serie:'Dorada', fecha:'12/07/2026', local:'Atletico Marista Dorados', gl:1, visita:'Juventus Dorados', gv:0, golesAM:[{n:'Cristian Cancino',g:1}] },
   { serie:'Diamante', fecha:'12/07/2026', local:'Atletico Marista Diamantes', gl:2, visita:'Juventus Diamantes', gv:1, golesAM:[{n:'German Ugalde',g:2}] },
   { serie:'Junior', fecha:'11/07/2026', local:'Atletico Marista Junior', gl:3, visita:'Ciclon Junior', gv:2, golesAM:[{n:'Baltazar Aroca',g:1},{n:'Nicolas Maldonado',g:1},{n:'Bastian Quilodran',g:1}] },
-  { serie:'Senior', fecha:'11/07/2026', local:'Ciclon Senior', gl:2, visita:'Atletico Marista Senior', gv:2, golesAM:[{n:'Ulises Galvez',g:1},{n:'Willy Jeno',g:1}] }
+  { serie:'Senior', fecha:'11/07/2026', local:'Ciclon Senior', gl:2, visita:'Atletico Marista Senior', gv:2 }
 ];
