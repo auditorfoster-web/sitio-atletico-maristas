93ba796797e57ded1bc2fadcf69282daf1ffce28
GitHub Action <action@github.com>
2026-08-26 22:46:12 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -2,8 +2,5 @@
 // Ultimo resultado jugado de cada serie (Atletico Maristas).
 window.RESULTADOS = [
   { serie:'Junior', fecha:'22/08/2026', local:'Atletico Marista Junior', gl:10, visita:'Juventus Junior', gv:0, golesAM:[{n:'Baltazar Aroca',g:2},{n:'Daniel Candia',g:1},{n:'Julian Gonzalez',g:1},{n:'Marcelo Guerra',g:1},{n:'Javier Maldonado',g:2},{n:'Sebastian Olea',g:1},{n:'Ariel Viveros',g:2}] },
-  { serie:'Senior', fecha:'22/08/2026', local:'Atletico Marista Senior', gl:3, visita:'Charkaplax Fc Senior', gv:0, golesAM:[{n:'Daniel Candia',g:1},{n:'Ulises Galvez',g:2}] },
-  { serie:'Super Senior', fecha:'22/08/2026', local:'Doveramigos Super Senior', gl:3, visita:'Atletico Marista Super Senior', gv:0 },
-  { serie:'Dorada', fecha:'22/08/2026', local:'Doveramigos Dorados', gl:0, visita:'Atletico Marista Dorados', gv:1, golesAM:[{n:'Hervy Aroca',g:1}] },
-  { serie:'Diamante', fecha:'22/08/2026', local:'Doveramigos Diamantes', gl:0, visita:'Atletico Marista Diamantes', gv:0 }
+  { serie:'Senior', fecha:'22/08/2026', local:'Atletico Marista Senior', gl:3, visita:'Charkaplax Fc Senior', gv:0, golesAM:[{n:'Daniel Candia',g:1},{n:'Ulises Galvez',g:2}] }
 ];
