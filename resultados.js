590e9b76da484536e9cf39145dfd62364e047826
GitHub Action <action@github.com>
2026-09-08 22:24:08 +0000

    chore: actualizar standings y fixtures

--- resultados.js
+++ resultados.js
@@ -2,6 +2,8 @@
 // Ultimo resultado jugado de cada serie (Atletico Maristas).
 window.RESULTADOS = [
   { serie:'Super Senior', fecha:'06/09/2026', local:'Mapuches Super Senior', gl:1, visita:'Atletico Marista Super Senior', gv:0 },
+  { serie:'Dorada', fecha:'06/09/2026', local:'Estudiantes Dorados', gl:1, visita:'Atletico Marista Dorados', gv:1, golesAM:[{n:'Hervy Aroca',g:1}] },
+  { serie:'Diamante', fecha:'06/09/2026', local:'Estudiantes Diamantes', gl:2, visita:'Atletico Marista Diamantes', gv:0 },
   { serie:'Junior', fecha:'05/09/2026', local:'Atletico Marista Junior', gl:0, visita:'Dinastia Fc Junior', gv:2 },
-  { serie:'Senior', fecha:'22/08/2026', local:'Atletico Marista Senior', gl:3, visita:'Charkaplax Fc Senior', gv:0, golesAM:[{n:'Daniel Candia',g:1},{n:'Ulises Galvez',g:2}] }
+  { serie:'Senior', fecha:'05/09/2026', local:'Atletico Marista Senior', gl:2, visita:'Dinastia Fc Senior', gv:0, golesAM:[{n:'Daniel Candia',g:1},{n:'Ulises Galvez',g:1}] }
 ];
